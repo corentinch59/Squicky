3fd42a0ff5115c5041043274a3454c2f13b6b7e8
Théo Le Goc <theo@legoc.net>
2025-06-12 18:22:28 -0400

    WIP quequette

--- Assets/Scripts/QueteDisplay.cs
+++ Assets/Scripts/QueteDisplay.cs
@@ -5,17 +5,14 @@ using UnityEngine;
 public class QueteDisplay : MonoBehaviour
 {
     public Quete[] quetes;
-    [SerializeField]
-    private GameObject singleQuetePrefab;
     
     private void OnEnable()
     {
-        foreach (Quete quete in quetes)
-        {
-            GameObject queteDisplay = Instantiate(singleQuetePrefab, transform);
-            SingleQueteDisplay singleQueteDisplay = queteDisplay.GetComponent<SingleQueteDisplay>();
-            singleQueteDisplay.quete = quete;
-            singleQueteDisplay.QueteUpdate();
-        }
+        
+    }
+
+    public void Toggle()
+    {
+        gameObject.SetActive(gameObject.activeSelf);
     }
 }
